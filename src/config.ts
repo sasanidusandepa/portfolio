5c846fec2c0913162fdd02405b3679eb6a6cf02e
Sasanidu Sandepa <sasanidu9@gmail.com>
2025-01-31 22:41:40 +0530

    Add personal resume and update header and profile information

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,15 @@
 
 
 export const SITE_FAVICON = "/portfolio/favicon.png";
-export const SITE_LOGO = "/portfolio/leo.png";
-export const SITE_TITLE = "LEO";
+export const SITE_LOGO = "/portfolio/profile.jpg";
+export const SITE_TITLE = "Sasanidu Sandepa";
 export const SITE_URL = "https://sasanidusandepa.github.io";
 export const BASE_PATH = "portfolio";
 export const SITE_DESCRIPTION =
   `
   This is a space where creativity meets logic. As a passionate frontend engineer and dreamer inspired by the vastness of space, Leo shares insights on cutting-edge web development, personal projects, and the exploration of new technologies. With a unique blend of technical expertise and a love for innovation, this blog is a journey into problem-solving, design, and the limitless possibilities of the digital universe.
 `;
-export const ME_AVATAR = "/portfolio/leo.png";
+export const ME_AVATAR = "/portfolio/profile.jpg";
 export const LANGUAGES = ['en', 'zh'];
 
 export const MENUS = [
@@ -31,7 +31,7 @@ export const MENUS = [
   },
 ];
 
-export const FOOTER_CONTENT = "Mr.Leo. All rights reserved.";
+export const FOOTER_CONTENT = "Sasanidu Sandepa. All rights reserved.";
 
 export const CONTACTS = {
   linkedIn: "YOUR_LINKEDIN_URL",
@@ -39,9 +39,9 @@ export const CONTACTS = {
   email: "mailto:YOUR_EMAIL",
 };
 
-export const SLOGAN = "To infinity and beyond!";
+export const SLOGAN = "Projects";
 export const ME_DESCRIPTION =
-  `Hi, I’m Leo! Since childhood, I’ve dreamed of becoming an astronaut, which shaped my passion for exploration and curiosity about the unknown. I’m a creative thinker with strong logical reasoning skills, allowing me to approach challenges innovatively yet systematically. I thrive on discovering new ideas and pushing boundaries, whether in my work or personal growth. My motto, “Explore endlessly, surpass yourself,” reflects my dedication to learning and achieving. Combining creativity with analytical thinking, I’m eager to contribute to meaningful projects and explore new opportunities!`;
+  `I am an ambitious, hardworking and dedicated individual. I consider myself to be a well-mannered and a reliable individual with a passion to succeed in life. My main aim is to learn and gain as much knowledge and experience as possible to progress my career. Im currently looking for the opportunity to start my career with an organization that will provide me with the right platform to demonstrate my abilities. The ideal position would allow me to develop new skills while strengthening those I already possess`;
 
 
 export const WORK_EXPERIENCE = [
@@ -51,44 +51,44 @@ export const WORK_EXPERIENCE = [
     startDate: "2022",
     endDate: "2024",
   },
-  
+
   {
     company: "Interstellar Academy Program",
     position: "Astro Trainee",
     startDate: "2023",
     endDate: "2023",
   },
-  
+
   {
     company: "LunarTech Solutions",
     position: "Creative Logic Engineer",
     startDate: "2020",
     endDate: "2022",
   },
-  
+
   {
     company: "Cosmic Coders Inc.",
     position: "Frontend Navigator",
     startDate: "2018",
     endDate: "2020",
   },
-  
+
 ];
 
 
 export const PROJECTS = [
-    {
-      id: "my-first-rocket-launch",
-      title: "My First Rocket Launch",
-      description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
-      image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      externalLink: "https://www.nasa.gov/",
-      details: [
-        "Explaining space exploration concepts through creative illustrations.",
-        "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
-        "Won an award in the interstellar creativity competition.",
-      ],
-    },
-  ];
-  
+  {
+    id: "my-first-rocket-launch",
+    title: "My First Rocket Launch",
+    description: "This is the first rocket launch I participated in. I was a member of the Interstellar Academy Program, and I was responsible for the rocket's navigation system. I was able to successfully navigate the rocket to the target destination.",
+    image: 'https://images.unsplash.com/photo-1516849841032-87cbac4d88f7?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    externalLink: "https://www.nasa.gov/",
+    details: [
+      "Explaining space exploration concepts through creative illustrations.",
+      "Making it easier for aspiring explorers to understand the basics of interstellar travel.",
+      "Won an award in the interstellar creativity competition.",
+    ],
+  },
+];
+
 export const GOOGLE_GTAG = "";
