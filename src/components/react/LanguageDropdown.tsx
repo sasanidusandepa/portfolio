8254cb8ee6378435654d47d991ddfc17bc1935ef
Sasanidu Sandepa <sasanidu9@gmail.com>
2025-02-07 23:37:03 +0530

    refactor: clean up imports and improve code formatting

--- src/components/react/LanguageDropdown.tsx
+++ src/components/react/LanguageDropdown.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { LANGUAGES } from "../../config";
 
 const Dropdown = () => {
--- src/content/config.ts
+++ src/content/config.ts
@@ -8,22 +8,22 @@ const blog = defineCollection({
       draft: z.boolean().optional(),
       hideOnHomePage: z.boolean().optional(),
       date: z
-      .string()
-      .or(z.date())
-      .transform((val) =>
-        new Date(val).toLocaleDateString("en-US", {
-          year: "numeric",
-          month: "short",
-          day: "numeric",
-        }),
-      ),
+        .string()
+        .or(z.date())
+        .transform((val) =>
+          new Date(val).toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          }),
+        ),
       tags: z.array(z.string()).optional(),
       // image or image url 
-      cover: image().optional().or(z.string()) ,
+      cover: image().optional().or(z.string()),
     }),
 });
 
 // in case we want to change the schema of the projects, we can do it here
 const projects = blog
 
-export const collections = { blog,projects };
+export const collections = { blog, projects };
